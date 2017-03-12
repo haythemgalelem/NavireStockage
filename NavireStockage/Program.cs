34e4b255ddf153a6e3870dd25eb9ceb2669de6d3
Julien RAVIA <jrgfawkes@gmail.com>
2017-03-12 19:25:18 +0100

    Ajout du fonctionnnement principal du programme

--- NavireStockage/Program.cs
+++ NavireStockage/Program.cs
@@ -10,6 +10,23 @@ namespace NavireStockage
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Navire unNavire = new Navire(159, "Drogue", "Slice of Life", "1337AB");
+
+                List<Stockage> Stockages = new List<Stockage>();
+                Stockages.Add(new Stockage(60));
+                Stockages.Add(new Stockage(100));
+
+                Port unPort = new Port(Stockages);
+                unPort.Dechargement(unNavire);
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex);
+            }
+
+            Console.ReadLine();
         }
     }
 }
