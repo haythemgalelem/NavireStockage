1fa007fb77809ef4d6eded28ebd0b0a839b8e17d
Julien RAVIA <jrgfawkes@gmail.com>
2017-03-12 16:28:24 +0100

    Ajout méthodes classe Stockage

--- NavireStockage/Stockage.cs
+++ NavireStockage/Stockage.cs
@@ -8,5 +8,55 @@ namespace NavireStockage
 {
     class Stockage
     {
+        private Int32 capaDispo;
+
+        /// <summary>
+        /// Retourner la capacité disponible dans la zone de stockage
+        /// </summary>
+        /// <returns></returns>
+        public Int32 ObtenirCapaDispo()
+        {
+            return capaDispo;
+        }
+
+        /// <summary>
+        /// Stocke la quantité qté et met a jour la capacité disponible
+        /// </summary>
+        /// <param name="pQté"></param>
+        public void Stocker(Int32 pQté)
+        {
+            this.capaDispo = capaDispo + pQté; 
+        }
+
+        /// <summary>
+        /// Indique si la zone de stockage est vide ou non
+        /// </summary>
+        /// <returns></returns>
+        public Boolean EstVide()
+        {
+            if(this.capaDispo == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Indique si la zone de stockage est remplie ou non
+        /// </summary>
+        /// <returns></returns>
+        public Boolean EstRemplie()
+        {
+            if (this.capaDispo == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
